529d3fa2582385ff6888f2541f2a97a74d862f17
nadz <mc4nadia@gmail.com>
2022-02-01 18:35:37 +0800

    change directory

--- benfranklin/pages/api/saveFile.ts
+++ benfranklin/pages/api/saveFile.ts
@@ -11,7 +11,7 @@ export default async function handler(
   ) {
       try{
         const text = req.body.text;
-        fs.writeFile('../public/text/text.txt', JSON.stringify(text), function(err:any) {
+        fs.writeFile('text.txt', JSON.stringify(text), function(err:any) {
           if(err) res.status(200).json({status: 'failed'});
           else res.status(200).json({status: 'success'});
         });
