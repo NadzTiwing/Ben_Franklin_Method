3b0c217789a0d4f4b97f04658c82b243303209dc
nadz <mc4nadia@gmail.com>
2022-02-01 18:28:43 +0800

    change directory

--- benfranklin/pages/api/readFile.ts
+++ benfranklin/pages/api/readFile.ts
@@ -11,7 +11,7 @@ export default async function handler(
     res: NextApiResponse<Res>
   ) {
       try{
-        fs.readFile('../benfranklin/public/text/text.txt', function(err:any, content:any) {
+        fs.readFile('../public/text/text.txt', function(err:any, content:any) {
           if(err) res.status(200).json({status: 'failed', text:""});
           else res.status(200).json({status: 'success', text:content.toString()});
         });
