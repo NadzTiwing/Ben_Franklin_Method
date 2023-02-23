e7e7911205477d278757798702d4d1421b9ea9ae
Nads <tiwingnadia@gmail.com>
2023-02-23 13:44:31 +0800

    change text input into rich text editor

--- main/src/lib/supabaseClient.js
+++ main/src/lib/supabaseClient.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient('https://kzrcjankjmtmjlqovvkf.supabase.co/', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cmNqYW5ram10bWpscW92dmtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0NDY2MDQsImV4cCI6MTk5MjAyMjYwNH0.VM6tl9jrxvJNbjNQYtRdLU4ThdCmc1HCf2Gr2Ncmni0');
-
+const URL = process.env.SUPABASE_URL;
+const API_KEY = process.env.API_KEY;
+export const supabase = createClient(URL, API_KEY);
--- main/src/pages/Home.tsx
+++ main/src/pages/Home.tsx
@@ -1,160 +1,201 @@
-import React, { useState, useEffect } from 'react';
-import {
-    Grid,
-    Button,
-    MenuItem,
-    Divider,
-    Container,   
-} from '@mui/material';
-import Textarea from '@mui/joy/Textarea';
-import EditIcon from '@mui/icons-material/Edit';
-import UploadFileIcon from '@mui/icons-material/UploadFile';
-import OptionMenu from '@/components/OptionMenu';
-import ArticlesModal from '@/components/ArticlesModal';
+import React, { useState, useEffect } from "react";
+import { Grid, Button, MenuItem, Divider, Container } from "@mui/material";
+import Textarea from "@mui/joy/Textarea";
+import EditIcon from "@mui/icons-material/Edit";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import OptionMenu from "@/components/OptionMenu";
+import ArticlesModal from "@/components/ArticlesModal";
 
+import { EditorState, convertToRaw } from "draft-js";
+// import { Editor } from "react-draft-wysiwyg";
 
+import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import dynamic from "next/dynamic";
 
 const articles = [
   {
-    id: 'uno',
-    title: "Article Uno"
+    id: "uno",
+    title: "Article Uno",
   },
   {
-    id: 'dos',
-    title: "Article Dos"
-  }
+    id: "dos",
+    title: "Article Dos",
+  },
 ];
 
 // let { data, error } = await supabase.from('countries').select()
-
-
 
 type Props = {
   data: string;
 };
 
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
+  {
+    ssr: false,
+  }
+);
+
 const Home = ({ data }: any) => {
-    
-    const getData = async () => {
-      
-      //const res = await fetch('https://catfact.ninja/fact');
-      //const countries = await data.json();
-      console.log(data);
-    }
+  const getData = async () => {
+    //const res = await fetch('https://catfact.ninja/fact');
+    //const countries = await data.json();
+    console.log(data);
+  };
 
-    useEffect(() => {
-      // console.log({DATA: countries})
-      // console.log({ERROR: error})
-      console.log(data)
-      //getData();
+  useEffect(() => {
+    // console.log({DATA: countries})
+    // console.log({ERROR: error})
+    console.log(data);
+    //getData();
+  }, []);
 
+  const [input, setInput] = useState(() => EditorState.createEmpty());
+  const [guideTxt, setGuideTxt] = useState(() => EditorState.createEmpty());
 
-    }, []);
+  //const [input, setInput] = useState<string>("");
+  //const [guideTxt, setGuideTxt] = useState<string>("");
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [isCreate, setCreate] = useState<boolean>(false);
+  const [isEdit, setEdit] = useState<boolean>(false);
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  const open = Boolean(anchorEl);
 
-    const [input, setInput] = useState<string>("");
-    const [guideTxt, setGuideTxt] = useState<string>("");
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const [isCreate, setCreate] = useState<boolean>(false);
-    const [isEdit, setEdit] = useState<boolean>(false);
-    const [isOpenModal, setOpenModal] = useState<boolean>(false);
-    const open = Boolean(anchorEl);
+  const onSubmit = () => {};
 
-    const onSubmit = () => {
+  const onCreateArticle = () => {
+    setEdit(!isEdit);
+    setCreate(!isCreate);
+    setAnchorEl(null);
+  };
 
-    }
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const onCreateArticle = () => {
-      setEdit(!isEdit);
-      setCreate(!isCreate);
-      setAnchorEl(null);
-    }
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
+  const onSave = () => {
+    setEdit(!isEdit);
+    setCreate(!isCreate);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const onEdit = () => {
+    setEdit(!isEdit);
+    setCreate(!isCreate);
+  };
 
-    const onSave = () => {
-      setEdit(!isEdit);
-      setCreate(!isCreate)
-    }
-
-    const onEdit = () => {
-      setEdit(!isEdit);
-      setCreate(!isCreate);
-    }
-
-    return(
-        <Grid container spacing={2} className="grid-container" >
-            <Grid item xs={12} >
-                <h3 className='title'>Writing Practice: Ben Franklin Method</h3>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Container>
-                    <Textarea placeholder="Enter your input here..." 
-                    className="write-form" 
-                    value={input} 
-                    onChange={(text) => setInput(text.target.value)}/>
-                    {!isCreate &&
-                      <Button variant="outlined" className="check-btn" size="large" onClick={onSubmit}>CHECK</Button>
-                    }
-                </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Container >
-                  <div className='option-btn'>
-                    <Button
-                        id="demo-customized-button"
-                        aria-controls={open ? 'demo-customized-menu' : undefined}
-                        aria-haspopup="true"
-                           aria-expanded={open ? 'true' : undefined}
-                        variant="contained"
-                        disableElevation
-                        onClick={handleClick}
-                    >
-                        Options
-                    </Button>
-                    <OptionMenu
-                        id="demo-customized-menu"
-                        MenuListProps={{
-                        'aria-labelledby': 'demo-customized-button',
-                        }}
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={() => onEdit()} disableRipple disabled={isEdit}>
-                          <EditIcon />
-                            Edit
-                        </MenuItem>
-                        <MenuItem onClick={() => onSave()} disableRipple disabled={!isCreate}>
-                          <UploadFileIcon />
-                            Save
-                        </MenuItem>
-                        <Divider sx={{ my: 0.5 }} />
-                        <MenuItem onClick={()=>setOpenModal(!isOpenModal)} disableRipple disabled={isCreate}>
-                          Change Article
-                        </MenuItem>
-                        <MenuItem onClick={() => onCreateArticle()} disableRipple disabled={isCreate}>
-                          New Article ( + )
-                        </MenuItem>
-                    </OptionMenu>
-                    <ArticlesModal isOpen={isOpenModal} setOpen={setOpenModal} articles={articles}/>
-                  </div>
-                  <h4 className={isCreate ? "green-txt" : ""}>Guide Section: Article Name</h4>
-                  <Textarea placeholder="Enter your guidance or clues here..." 
-                    className={isCreate ? "write-form guide-txt green-border":"write-form guide-txt"} 
-                    value={guideTxt} 
-                    onChange={(text) => setGuideTxt(text.target.value)}
-                    disabled={!isEdit}
-                  />
-                </Container>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container spacing={2} className="grid-container">
+      <Grid item xs={12}>
+        <h3 className="title">Writing Practice: Ben Franklin Method</h3>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Container>
+          <div className="write-form">
+            <Editor editorState={input} onEditorStateChange={setInput} />
+          </div>
+          {!isCreate && (
+            <Button
+              variant="outlined"
+              className="check-btn"
+              size="large"
+              onClick={onSubmit}
+            >
+              CHECK
+            </Button>
+          )}
+        </Container>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Container>
+          <div className="option-btn">
+            <Button
+              id="demo-customized-button"
+              aria-controls={open ? "demo-customized-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              variant="contained"
+              disableElevation
+              onClick={handleClick}
+            >
+              Options
+            </Button>
+            <OptionMenu
+              id="demo-customized-menu"
+              MenuListProps={{
+                "aria-labelledby": "demo-customized-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem
+                onClick={() => onEdit()}
+                disableRipple
+                disabled={isEdit}
+              >
+                <EditIcon />
+                Edit
+              </MenuItem>
+              <MenuItem
+                onClick={() => onSave()}
+                disableRipple
+                disabled={!isCreate}
+              >
+                <UploadFileIcon />
+                Save
+              </MenuItem>
+              <Divider sx={{ my: 0.5 }} />
+              <MenuItem
+                onClick={() => setOpenModal(!isOpenModal)}
+                disableRipple
+                disabled={isCreate}
+              >
+                Change Article
+              </MenuItem>
+              <MenuItem
+                onClick={() => onCreateArticle()}
+                disableRipple
+                disabled={isCreate}
+              >
+                New Article ( + )
+              </MenuItem>
+            </OptionMenu>
+            <ArticlesModal
+              isOpen={isOpenModal}
+              setOpen={setOpenModal}
+              articles={articles}
+            />
+          </div>
+          <h4 className={isCreate ? "green-txt" : ""}>
+            Guide Section: Article Name
+          </h4>
+          <div className="write-form">
+            <Editor
+              editorState={guideTxt}
+              onEditorStateChange={setGuideTxt}
+              readOnly={!isEdit}
+              toolbarHidden={!isEdit}
+            />
+          </div>
+          {/* <Textarea
+            placeholder="Enter your guidance or clues here..."
+            className={
+              isCreate
+                ? "write-form guide-txt green-border"
+                : "write-form guide-txt"
+            }
+            value={guideTxt}
+            onChange={(text) => setGuideTxt(text.target.value)}
+            disabled={!isEdit}
+          /> */}
+        </Container>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Home;
